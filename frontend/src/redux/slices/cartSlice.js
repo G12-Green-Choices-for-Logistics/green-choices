7462778bb36f2e4656ab1619e390960e98726da1
winonakfernandes <winona.fernandes@tuni.fi>
2025-03-04 16:06:37 +0200

    Update cartSlice.js

--- frontend/src/redux/slices/cartSlice.js
+++ frontend/src/redux/slices/cartSlice.js
@@ -1,43 +1,43 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  items: [
-    {
-      img: '/imgs/sneakers.png',
-      name: "Caliber Canvas - Seude",
-      size: "EU 44",
-      color: "Beige",
-      price: 80.00,
-      quantity: 1,
-    },
-    {
-      img: '/imgs/sneakers.png',
-      name: "Caliber Canvas - Seude",
-      size: "EU 44",
-      color: "Beige",
-      price: 80.00,
-      quantity: 1,
-    },
-  ],
+    items: [
+        {
+            img: '/imgs/sneakers.png',
+            name: "Caliber Canvas - Seude",
+            size: "EU 44",
+            color: "Beige",
+            price: 80.00,
+            quantity: 1,
+        },
+        {
+            img: '/imgs/sneakers.png',
+            name: "Caliber Canvas - Seude",
+            size: "EU 44",
+            color: "Beige",
+            price: 80.00,
+            quantity: 1,
+        }
+    ],
 };
 
 const cartSlice = createSlice({
-  name: 'cart',
-  initialState,
-  reducers: {
-    addItem: (state, action) => {
-      state.items.push(action.payload);
+    name: 'cart',
+    initialState,
+    reducers: {
+        addItem: (state, action) => {
+            state.items.push(action.payload);
+        },
+        removeItem: (state, action) => {
+            state.items = state.items.filter((item, index) => index !== action.payload);
+        },
+        updateQuantity: (state, action) => {
+            const { index, quantity } = action.payload;
+            if (quantity > 0) {
+                state.items[index].quantity = quantity;
+            }
+        },
     },
-    removeItem: (state, action) => {
-      state.items = state.items.filter((item, index) => index !== action.payload);
-    },
-    updateQuantity: (state, action) => {
-      const { index, quantity } = action.payload;
-      if (quantity > 0) {
-        state.items[index].quantity = quantity;
-      }
-    },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = cartSlice.actions;
